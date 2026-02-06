9bf4ade59e6a7d4da1a0643d5dfd6a4201f592e8
Deepak Singh <deepakksingh1202@gmail.com>
2026-02-06 23:18:38 +0530

    Update post schema

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,28 +1,49 @@
 import mongoose, { Schema } from "mongoose";
 
 const postSchema = new Schema(
-    {
-        title: {
-            type: String,
-            required: true
-        },
-        content: {
-            type: String,
-            required: true
-        },
-        discription: {
-            type: String,
-            required: true,
-        },
-        isPublished: {
-            type: Boolean,
-            default: true
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    }, { timestamps: true }
-)
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
 
-export const Post = mongoose.model("Post", postSchema)
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+
+    content: {
+      type: String,
+      required: true
+    },
+
+    metaTitle: {
+      type: String,
+      required: true,
+      maxlength: 60
+    },
+
+    metaDescription: {
+      type: String,
+      required: true,
+      maxlength: 160
+    },
+
+    isPublished: {
+      type: Boolean,
+      default: true
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model("Post", postSchema);
